5027f35302c03d39ff58bf7ad73558bfc333d632
dotnet-code <@dotnet-code>
2023-04-17 14:32:43 +0530

    commit from code

---
+++ test_app/Controllers/JiraController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Test_App.Configuration;
+
+namespace Test_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JiraController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetJiraConfiguration()
+        {
+            try
+            {
+                var jiraConfiguration = Jira.GetConfiguration();
+                return Ok(jiraConfiguration);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
