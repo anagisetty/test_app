ee9f08910227c7b31adfbe339141fc4c5228b36e
dotnet-code <@dotnet-code>
2023-04-18 11:15:27 +0530

    commit from code202304181527

---
+++ test_app/Controllers/ConfigController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace test_app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {        
+        private IConfiguration _configuration;
+        public ConfigController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public IActionResult GetGithubConfiguration()
+        {
+            var config = new {
+                Name = _configuration["GitHub:Name"],
+                Email = _configuration["GitHub:Email"],
+                Username = _configuration["GitHub:Username"],
+                Password = _configuration["GitHub:Password"],
+                Repository = _configuration["GitHub:Repository"]
+            };
+
+            return Ok(config);
+        }
+    }
+}
