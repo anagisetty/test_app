fcfb119b797ff8e014af1027530e7c689e2fa4c6
dotnet-code <@dotnet-code>
2023-04-18 11:00:50 +0530

    commit from code202304180051

---
+++ test_app/Controllers/ConfigurationJiraController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Test_App.Models;
+
+namespace Test_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigurationJiraController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetConfigurationDetails()
+        {
+            //Get Configuration Details from database
+            ConfigurationData configData = new ConfigurationData();
+            try
+            {
+                //Process the data
+                return Ok(configData);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult UpdateConfigurationDetails(ConfigurationData configData)
+        {
+            try
+            {
+                //Update Configuration Details into database
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
