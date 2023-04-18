a2dc75d2d4d9fec61e8a2ae765d2dcabe3d7096c
dotnet-code <@dotnet-code>
2023-04-18 19:17:15 +0530

    commit from code202304181715

--- test_app/Controllers/JiraController.cs
+++ test_app/Controllers/JiraController.cs
@@ -1,27 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 using test_app.Models;
 
 namespace test_app.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class JiraController : ControllerBase
+    public class JiraController : ApiController
     {
-        public JiraController()
+        [HttpPost]
+        public HttpResponseMessage ConfigureJira(JiraConfiguration configuration)
         {
-        }
+            //Validate the configuration
+            if (configuration == null || string.IsNullOrWhiteSpace(configuration.Url) || string.IsNullOrWhiteSpace(configuration.Username) || string.IsNullOrWhiteSpace(configuration.Password))
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Required parameter missing.");
+            }
 
-        [HttpGet]
-        [Route("configure")]
-        public ActionResult ConfigureJira()
-        {
-            //Code for configuration of JIRA
-            return new OkObjectResult("JIRA configured successfully");
+            //Configure Jira
+            try
+            {
+                //Connect to Jira
+                JiraConnection con = new JiraConnection(configuration.Url, configuration.Username, configuration.Password);
+                con.Connect();
+
+                //Validate the connection
+                if (!con.IsConnected)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Unable to connect to Jira.");
+                }
+
+                //Validate the credentials
+                if (!con.ValidateCredentials())
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid credentials.");
+                }
+
+                //If all validations are successful, save the configuration
+                configuration.Save();
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
         }
     }
 }
