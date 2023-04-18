4c4a366c4af229d34b05cf2e4cec03880e11e672
dotnet-code <@dotnet-code>
2023-04-18 18:01:43 +0530

    commit from code202304180143

---
+++ test_app/Controllers/JiraConfigController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Http;
+using test_app.Models.Configuration;
+
+namespace test_app.Controllers
+{
+    public class JiraConfigController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult ConfigureJira([FromBody]JiraConfig config)
+        {
+            try
+            {
+                // Logic to configure Jira
+                return Ok("Jira configuration successful");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Failed to configure Jira: " + ex.Message);
+            }
+        }
+    }
+}
