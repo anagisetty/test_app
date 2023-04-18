a7f8793739d509c9837262e235a49fba3d823c5b
dotnet-code <@dotnet-code>
2023-04-18 11:03:40 +0530

    commit from code202304180340

---
+++ test_app/Controllers/JiraConfigurationController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Http;
+
+namespace test_app
+{
+    public class JiraConfigurationController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetJiraConfiguration()
+        {
+            try
+            {
+                //Get Jira Configuration
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult UpdateJiraConfiguration(JiraConfiguration jiraConfiguration)
+        {
+            try
+            {
+                //Update Jira Configuration
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+
+    public class JiraConfiguration
+    {
+        public string Url { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
